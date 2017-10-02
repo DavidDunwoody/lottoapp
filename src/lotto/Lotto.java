ce4c27bf8eeaece70f02e3f7060415839673a5c6
EJ <lefyen@gmail.com>
2017-10-02 17:56:24 +0100

    Update
    first and final commit

---
+++ src/lotto/Lotto.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lotto;
+
+/**
+ *
+ * @author x15316351
+ */
+public class Lotto {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+       FirstGui Fgui = new FirstGui();
+       Fgui.setVisible(true);
+        
+        //LottoGUI Lgui = new LottoGUI();
+        //Lgui.setVisible(true);
+        
+        //Plus1GUI Pgui = new Plus1GUI();
+        //Pgui.setVisible(true);
+        
+    }
+    
+}
